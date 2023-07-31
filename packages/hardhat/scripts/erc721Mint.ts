89240e1114c8ef0be564feeaa1718b5f91970e16
xi-one <ssly1133@naver.com>
2023-07-31 16:58:10 +0900

    add erc1155 deploy script

--- packages/hardhat/scripts/erc721Mint.ts
+++ packages/hardhat/scripts/erc721Mint.ts
@@ -12,7 +12,7 @@ const ethersSigner = walletObj.connect(provider);
 async function main() {
     const receiver = "0xc75C8C7f741a312Ba9f5E6725cf837EcB379054D";
     const ERC721Contract = new ethers.Contract(ERC721Token.address, ERC721Token.abi, ethersSigner);
-    const mintTx = await ERC721Contract.mintNFT(receiver, " ", 0);
+    /* const mintTx = await ERC721Contract.mintNFT(receiver, " ", 0);
 
     const mintRc = await mintTx.wait();
     const mintEvent = mintRc.events.find((element: any) => element.event === 'MintNFT');
@@ -22,7 +22,10 @@ async function main() {
     //console.log(mintRc);
     const addr = await ERC721Contract.getAccountAddress(tokenId, 0);
 
-    console.log("address check: ", addr);
+    console.log("address check: ", addr); */
+
+    const result = await ERC721Contract.isActive(1);
+    console.log(result);
 
     /* const TokenId = await ERC721Contract.supply();
     console.log("Minted id: ", TokenId);
