470636ee4c7dfcac31bee27b8d518996a85a23dd
xi-one <ssly1133@naver.com>
2023-07-31 16:27:21 +0900

    feat: add erc1155 Token

---
+++ packages/hardhat/contracts/ERC1155Token.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ERC1155Token is ERC1155, Ownable {
+    constructor() ERC1155("https://bafybeidfaynugl6uhxvgsawncisfzle2banmwkieqt5d7zxsqhjkfr6aiy.ipfs.nftstorage.link/{id}.json") {}
+
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+
+    function uri(uint256 _tokenid) override public pure returns (string memory) {
+        return string(
+            abi.encodePacked(
+                _uri,
+                Strings.toString(_tokenid),".json"
+            )
+        );
+    }
+}
