2e22695a1db53dc7a8d27aac3e1bef2d1adde2b8
Me1e <mele0404@naver.com>
2023-07-31 17:13:14 +0900

    feat(home): map component

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -1,15 +1,12 @@
-import Link from "next/link";
 import type { NextPage } from "next";
-import { BugAntIcon, MagnifyingGlassIcon, SparklesIcon } from "@heroicons/react/24/outline";
+import Map from "~~/components/Map";
 import { MetaHeader } from "~~/components/MetaHeader";
 
 const Home: NextPage = () => {
   return (
     <>
       <MetaHeader />
-      <div className="flex items-center flex-col flex-grow pt-10">
-        
-      </div>
+      <Map />
     </>
   );
 };
