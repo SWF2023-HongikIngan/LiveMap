75a9271e61785e8578873ff85bcc11a10b23dce8
Me1e <mele0404@naver.com>
2023-08-01 09:15:35 +0900

    feat(css): tailwind 제거

--- packages/nextjs/pages/_app.tsx
+++ packages/nextjs/pages/_app.tsx
@@ -43,9 +43,9 @@ const ScaffoldEthApp = ({ Component, pageProps }: AppProps) => {
           avatar={BlockieAvatar}
           theme={isDarkTheme ? darkTheme() : lightTheme()}
         >
-          <div className="flex flex-col min-h-screen">
-            <Header />
-            <main className="relative flex flex-col flex-1">
+          <div>
+            {/* <Header /> */}
+            <main>
               <Component {...pageProps} />
             </main>
             {/* <Footer /> */}
