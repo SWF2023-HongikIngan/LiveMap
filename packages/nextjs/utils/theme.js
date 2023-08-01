90c962bbc440c74866863812b66082e8b5d2a6af
Me1e <mele0404@naver.com>
2023-08-01 11:40:26 +0900

    feat(report): report 양식 완성

--- packages/nextjs/utils/theme.js
+++ packages/nextjs/utils/theme.js
@@ -1,56 +1,55 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 const darkTheme = createTheme({
-    palette: {
-        // mode: 'dark',
-        mode: 'light',
-        primary: {
-            main: "#F0FF42"
-        },
-        secondary: {
-            main: "#474d57"
-        },
-        background: {
-            paper: "#0b0e11",
-            default: "#181a20"
-        },
-
+  palette: {
+    // mode: 'dark',
+    mode: "light",
+    primary: {
+      main: "#000000",
     },
+    secondary: {
+      main: "#FF7D54",
+    },
+    background: {
+      paper: "#0b0e11",
+      default: "#181a20",
+    },
+  },
 });
 
 export default darkTheme;
 
-export const primaryColor = "#fcd535"
-export const secondaryColor = "#474d57"
+export const primaryColor = "#fcd535";
+export const secondaryColor = "#474d57";
 
 export const colorObj = {
+  primary: "#fcd535",
+  secondary: "#474d57",
+  background: {
+    paper: "#0b0e11",
+    default: "#181a20",
+  },
+  text: {
     primary: "#fcd535",
     secondary: "#474d57",
-    background: {
-        paper: "#0b0e11",
-        default: "#181a20"
-    },
-    text: {
-        primary: "#fcd535",
-        secondary: "#474d57",
-        disabled: "#474d57",
-        hint: "#474d57",
-    },
-    action: {
-        active: "#fcd535",
-        hover: "#fcd535",
-        hoverOpacity: 0.08,
-        selected: "#fcd535",
-        selectedOpacity: 0.16,
-        disabled: "#474d57",
-        disabledBackground: "#474d57",
-        disabledOpacity: 0.38,
-        focus: "#fcd535",
-        focusOpacity: 0.12,
-        activatedOpacity: 0.24,
-    },
-    divider: "#474d57",
-}
+    disabled: "#474d57",
+    hint: "#474d57",
+  },
+  action: {
+    active: "#fcd535",
+    hover: "#fcd535",
+    hoverOpacity: 0.08,
+    selected: "#fcd535",
+    selectedOpacity: 0.16,
+    disabled: "#474d57",
+    disabledBackground: "#474d57",
+    disabledOpacity: 0.38,
+    focus: "#fcd535",
+    focusOpacity: 0.12,
+    activatedOpacity: 0.24,
+  },
+  divider: "#474d57",
+};
 
 // 메인
 //#fcd535
