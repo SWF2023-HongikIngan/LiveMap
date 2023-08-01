5e2a6d39c64dfe6f80f1db3ef2a6fe52e162aba1
smarthug <kirklayer@gmail.com>
2023-08-01 11:06:05 +0900

    shadow added

--- packages/nextjs/components/Appbar.tsx
+++ packages/nextjs/components/Appbar.tsx
@@ -15,7 +15,6 @@ import Toolbar from "@mui/material/Toolbar";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 import { Bars3Icon } from "@heroicons/react/24/outline";
 
-
 export default function ResponsiveDrawer() {
   const [mobileOpen, setMobileOpen] = React.useState(false);
 
@@ -30,13 +29,15 @@ export default function ResponsiveDrawer() {
           width: { sm: `100%` },
 
           background: "rgba(255, 255, 255, 0.1)",
-          boxShadow: `0 8px 32px 0 rgba(255, 255, 255, 0.17)`,
+        //   boxShadow: `0 8px 32px 0 rgba(255, 255, 255, 0.17)`,
+          boxShadow: `5px 5px 5px #888888`,
+        //   box-shadow: 5px 5px 5px #888888;
           backdropFilter: `blur(20px)`,
           WebkitBackdropFilter: `blur(20px)`,
           border: `1px solid rgba(255, 255, 255, 0.18)`,
           color: "white",
           padding: 0,
-        //   borderRadius: 20,
+          //   borderRadius: 20,
           overflow: "hidden",
         }}
       >
