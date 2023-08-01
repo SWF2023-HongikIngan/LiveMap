b95e89a45becbcf5809c673e8a56cec638685819
xi-one <ssly1133@naver.com>
2023-08-01 19:26:50 +0900

    deploy:fixed 1155contract

--- packages/hardhat/scripts/erc1155Mint.ts
+++ packages/hardhat/scripts/erc1155Mint.ts
@@ -1,26 +1,30 @@
 import { ethers } from 'hardhat'
-//import ERC1155Token from '../deployments/cronos_testnet/ERC1155Token.json';
-import ERC1155Token from '../deployments/localhost/ERC1155Token.json';
+import ERC1155Token from '../deployments/cronos_testnet/ERC1155Token.json';
+//import ERC1155Token from '../deployments/localhost/ERC1155Token.json';
 
-
-/* const provider = new ethers.providers.JsonRpcProvider(
-    "https://evm-t3.cronos.org/"
-); */
 
 const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:8545/"
+    "https://evm-t3.cronos.org/"
 );
-//const secretkey = process.env.DEPLOYER_PRIVATE_KEY;
-const secretkey = process.env.LOCAL_PRIVATE_KEY;
+
+/* const provider = new ethers.providers.JsonRpcProvider(
+    "http://127.0.0.1:8545/"
+); */
+const secretkey = process.env.DEPLOYER_PRIVATE_KEY;
+//const secretkey = process.env.LOCAL_PRIVATE_KEY;
 const walletObj = new ethers.Wallet(secretkey!);
 const ethersSigner = walletObj.connect(provider);
 
 async function main() {
     const ERC1155Contract = new ethers.Contract(ERC1155Token.address, ERC1155Token.abi, ethersSigner);
-    const mintTx = await ERC1155Contract.mint("0x2b2940d4888effa1918C85D10F0740A90c447aa7", 2, []);
+    for(let i = 1; i <= 5; i++){
+        const mintTx = await ERC1155Contract.mint("0xbf8201E851f691ABe6B765e300256DC8A2024F15", i, []);
+        const mintRc = await mintTx.wait();
+    }
+    
 
-    const mintRc = await mintTx.wait();
-    console.log(mintRc);
+    
+   
 
     const tokens = await ERC1155Contract.getTokens(1, 0);
     console.log(tokens);
